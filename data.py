3088a11a5b9f7053b9106555885d23ca38d6e614
19146 <42159620+19146@users.noreply.github.com>
2023-08-13 10:05:10 +0530

    Update data.py
    Removed beta

--- data.py
+++ data.py
@@ -7,7 +7,7 @@ def app(car_df):
     # Displaying orginal dataset
     st.header("View Data")
     # Add an expander and display the dataset as a static table within the expander.
-    with st.beta_expander("View Dataset"):
+    with st.expander("View Dataset"):
         st.table(car_df)
 
     # Display descriptive statistics.
@@ -17,7 +17,7 @@ def app(car_df):
     
     # ADD NEW CODE FROM HERE
     # Add a subheader and create three columns. Store the columns in two separate variables.
-    beta_col1, beta_col2, beta_col3 = st.beta_columns(3)
+    beta_col1, beta_col2, beta_col3 = st.columns(3)
 
     # Add a checkbox in the first column. Display the column names of 'car_df' on the click of checkbox.
     with beta_col1:

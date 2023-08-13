a08bffff7c569ffc742e3b3ce86525fcdd6bd760
19146 <42159620+19146@users.noreply.github.com>
2023-08-13 09:12:45 +0530

    Update data.py
    Reverted to without beta

--- data.py
+++ data.py
@@ -17,7 +17,7 @@ def app(car_df):
     
     # ADD NEW CODE FROM HERE
     # Add a subheader and create three columns. Store the columns in two separate variables.
-    beta_col1, beta_col2, beta_col3 = st.beta_columns(3)
+    beta_col1, beta_col2, beta_col3 = st.columns(3)
 
     # Add a checkbox in the first column. Display the column names of 'car_df' on the click of checkbox.
     with beta_col1:
